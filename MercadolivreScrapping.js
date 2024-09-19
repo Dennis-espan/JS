77d4f0d69f0bf242cfe0da6af6ccb2c71edf540f
Dennis-espan <141745471+Dennis-espan@users.noreply.github.com>
2024-09-18 22:03:16 -0300

    Webscrapping
    o seguinte trecho de codigo retorna os preços dos produtos da pagina inicial desde que sejam menores que 300R$

---
+++ MercadolivreScrapping.js
@@ -0,0 +1,15 @@
+// O seguinte comando deve ser usado em https://www.mercadolivre.com.br/
+// O mesmo busca por produtos na pagina inicial e retornas seus preços desde que sejam menores que 300R$
+
+const produto=document.querySelectorAll('.andes-money-amount.andes-money-amount--cents-superscript');
+
+// Filtra e exibe os preços abaixo de 300R$
+produto.forEach(pspan => {
+    const ptexto = pspan.textContent.trim(); // Remove espaços em branco
+    const preco = parseFloat(ptexto.replace('R$', '').replace('.', '').replace(',', '.').trim()); // Formata o preço
+
+    if (preco < 300) {
+        console.log(pspan); // Exibe o elemento
+        console.log(ptexto); // Exibe o conteúdo do elemento (no caso o preço)
+    }
+});
